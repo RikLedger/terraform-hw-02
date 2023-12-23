43ad647efd290ec035e45016f33ceef95fb2ae79
RikLedger <131041498+RikLedger@users.noreply.github.com>
2023-12-23 17:58:19 +0300

    Update locals.tf

--- SRC/locals.tf
+++ SRC/locals.tf
@@ -1,13 +1,4 @@
 locals {
-  project = "netology-develop-platform"
-  env_web = "web"
-  env_db  = "db"
   vm_web_instance_name = "${local.project}-${local.env_web}"
   vm_db_instance_name = "${local.project}-${local.env_db}"
-}
-
-locals {
-  org      = "netology"
-  project  = "develop"
-  instance = "platform"
 }
